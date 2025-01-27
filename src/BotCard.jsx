be3d5c38b2fe301d11485909e479015514fec2e0
Engineer-Collo <collinslikhomba@gmail.com>
2025-01-27 08:00:42 +0300

    initial commit

--- src/BotCard.jsx
+++ src/BotCard.jsx
@@ -4,9 +4,16 @@ const BotCard = ({ bot, handleClick, dischargeBot }) => {
   return (
     <div className="bot-card" onClick={handleClick}>
       <img src={bot.avatar_url} alt={bot.name} />
-      <h3>{bot.name}</h3>
+      <h3>Name:{bot.name}</h3>
       <p>Health: {bot.health}</p>
       <p>Armor: {bot.armor}</p>
+      <p>Damage: {bot.damage} </p>
+      <p>Bot_class: {bot.bot_class} </p>
+      <p>created_at: {bot.created_at} </p>
+      <p>updated_at: {bot.updated_at} </p>
+
+
+
       {dischargeBot && (
         <button onClick={(e) => { 
           e.stopPropagation(); 
--- src/BotsCollection.jsx
+++ src/BotsCollection.jsx
@@ -4,7 +4,7 @@ import BotCard from './BotCard';
 const BotCollection = ({ bots, addToArmy }) => {
   return (
     <div>
-      <h2>All Bots</h2>
+      <h2>The Bots Collection</h2>
       <div className="bot-collection">
         {bots.map((bot) => (
           <BotCard key={bot.id} bot={bot} handleClick={() => addToArmy(bot)} />

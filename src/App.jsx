df84b6a514b00a6e416583cf3e1af75f329d4358
Engineer-Collo <collinslikhomba@gmail.com>
2025-01-30 15:37:18 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [bots, setBots] = useState([]);
   const [army, setArmy] = useState([]);
-
+  
+// fetching data from the backend.
   useEffect(() => {
-    fetch("http://localhost:3001/bots")
+    fetch("https://bots-si0g.onrender.com/bots")
       .then((res) => res.json())
       .then((data) => setBots(data));
   }, []);
 
+  //adding bot to the army list
   const addToArmy = (bot) => {
     if (!army.some((b) => b.id === bot.id)) {
       setArmy([...army, bot]);
     }
   };
 
+  //removing the bot from the army list
   const removeFromArmy = (bot) => {
     setArmy(army.filter((b) => b.id !== bot.id));
   };
 
+  //delete the bot permanently on the backend.
   const dischargeBot = (botId) => {
-    fetch(`http://localhost:3001/bots/${botId}`, { method: 'DELETE' })
+    fetch(`https://bots-si0g.onrender.com/bots/${botId}`, { method: 'DELETE' })
       .then(() => {
         setBots(bots.filter((bot) => bot.id !== botId));
         setArmy(army.filter((b) => b.id !== botId));
